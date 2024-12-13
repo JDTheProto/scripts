77e283e42e671b7b92e6121ae813471b180d3a05
JDTheProto <134808178+JDTheProto@users.noreply.github.com>
2024-12-13 19:17:17 +0100

    Hello world script
    este script es super sigma

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Hola skibidi sigma del futuro, esta linea te dice hola
+        print("Hello, Daniel!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
